b5b15406d60e5cefa37c196b30f5cc3286877778
Sean Leavey <sean.leavey@gmail.com>
2016-03-02 21:17:32 +0000

    Fixed potential bug with buffer overflow

--- magnetometer/save-calibrated.py
+++ magnetometer/save-calibrated.py
@@ -91,6 +91,9 @@ last_line_length = None
 
 # now loop, printing the data received from the ADC
 while True:
+    # sleep for specified time
+    time.sleep(sleep_time)
+    
     # get data
     data = server.get_command_response("dataafter {0}".format(timestamp))
 
@@ -105,12 +108,13 @@ while True:
 
             continue
 
-        # check if buffer length has been reached
-        if len(datalist) >= 2 and len(datalist[-1]) is not len(datalist[-2]):
-            # last row doesn't have the same number of columns as second last
-            # this indicates the buffer length was reached
-            # discard last row (it will be fetched next time)
-            del(datalist[-1])
+        # delete last row to prevent buffer overflow issues
+        del(datalist[-1])
+        
+        # without the last entry, do we have any new readings?
+        if len(datalist) < 1:
+            # skip this iteration
+            continue
 
         # loop over data, converting the counts to volts
         for i in xrange(len(datalist)):
@@ -140,6 +144,3 @@ with enabled channels".format(i))
     else:
         print("Skipped empty data from server. Timestamp: {0}, \
 received data: {1}".format(timestamp, data))
-
-    # sleep for specified time
-    time.sleep(sleep_time)
